b49043ad3d8b7e354075bcab489db365a6fc6a7e
Akoun225 <Akoun.aboa@uvci.edu.ci>
2023-06-26 17:12:45 +0100

    Module vide; sans fonctionnalité

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Nom du module',
+    'summary': "Information sur le module",
+    'version': '1.0',
+    'category': '',
+    'author': 'Aboa Akoun Bernard',
+    'maintainer': 'Aboa Akoun Bernard',
+    'depends': [
+        # Les dependance du module
+        'base',
+        'mail',
+        'calendar',
+    ],
+    'data': [
+        # Les vues
+        # 'views/assets.xml',
+    ],
+    # Autre parametres du module
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
